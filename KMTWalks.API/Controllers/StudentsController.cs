438ead486dafe8a60a964d38087d31d8a8278752
kamto-s <kamto.076s@gmail.com>
2025-07-06 17:15:24 +0700

    Add project files.

---
+++ KMTWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KMTWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "David", "Siti", "Wawan", "Bambang" };
+
+            return Ok(studentNames);
+        }
+    }
+}
